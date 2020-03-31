bb4d3c46dbcac24737cd9ad94846b2ff45992977
tiagosn01 <tiagosilva0922@gmail.com>
2020-03-31 19:26:34 -0300

    Configurações de estilização e rotas

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-boost-masculino/08/COL-7905-008/COL-7905-008_zoom2.jpg?ts=1576605177&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas Boost</strong>
+        <span> R$ 450,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
